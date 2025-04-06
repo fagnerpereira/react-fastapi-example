8b1bd71cb183e4ff949b9d26e61cf7c073e20071
Fagner Pereira Rosa <fagnerfpr@gmail.com>
2025-04-06 19:10:22 -0300

    add fruits crud and tests

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,87 @@
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from datetime import datetime
+from starlette.middleware.cors import CORSMiddleware
+from itertools import count
 
+origins = ["http://localhost:5173"]
 app = FastAPI()
-
-
-class Fruit(BaseModel):
-    name: str
-
-
-class Fruits(BaseModel):
-    fruits: List
-
-
-fruits_db = Fruits(
-    fruits=[
-        Fruit(name="apple"),
-        Fruit(name="banana"),
-        Fruit(name="orange"),
-    ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
-@app.get("/fruits")
+class Fruit(BaseModel):
+    id: int
+    name: str
+    created_at: datetime
+
+
+class CreateFruit(BaseModel):
+    name: str
+
+
+class Fruits(dict):
+    pass
+
+
+fruits_db = Fruits(
+    {
+        1: Fruit(id=1, name="apple", created_at=datetime.now()),
+        2: Fruit(id=2, name="banana", created_at=datetime.now()),
+        3: Fruit(id=3, name="orange", created_at=datetime.now()),
+    }
+)
+fruit_id_counter = count(4)
+
+
 def get_fruits():
-    return fruits_db
+    return fruits_db.values()
+
+
+@app.get("/fruits")
+def read_fruits() -> List:
+    return get_fruits()
+
+
+@app.get("/fruits/{fruit_id}")
+def read_fruit(fruit_id: int) -> Fruit:
+    return fruits_db[fruit_id]
+
+
+@app.post("/fruits")
+def create_fruits(fruit: CreateFruit) -> Fruit:
+    new_id = next(fruit_id_counter)
+    new_fruit = Fruit(id=new_id, name=fruit.name, created_at=datetime.now())
+    fruits_db[new_id] = new_fruit
+
+    return new_fruit
+
+
+@app.put("/fruits/{fruit_id}")
+def update_fruit(fruit_id: int, fruit: CreateFruit) -> Fruit:
+    if fruit_id in fruits_db:
+        fruits_db[fruit_id].name = fruit.name
+        return fruits_db[fruit_id]
+    else:
+        return {"error": "Fruit not found"}, 404
+
+
+@app.delete("/fruits/{fruit_id}")
+def delete_fruit(fruit_id: int):
+    if fruit_id in fruits_db:
+        name = fruits_db[fruit_id].name
+        del fruits_db[fruit_id]
+        return {"message": "Fruit " + name + " deleted"}
+    else:
+        return {"error": "Fruit not found"}, 404
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- backend/test_main.py
+++ backend/test_main.py
@@ -1,12 +1,45 @@
 from fastapi.testclient import TestClient
-from .main import app
+from .main import app, fruits_db
+from typing import List
 
 client = TestClient(app)
 
 
-def test_get_fruits():
+def test_read_fruits():
     response = client.get("/fruits", headers={})
+    fruits = response.json()
+
     assert response.status_code == 200
-    assert response.json() == {
-        "fruits": [{"name": "apple"}, {"name": "banana"}, {"name": "orange"}]
-    }
+    assert isinstance(fruits, List)
+
+
+def test_read_fruit():
+    response = client.get("/fruits/1")
+    fruit = response.json()
+
+    assert response.status_code == 200
+    assert fruit["id"] == 1
+
+
+def test_create_fruits():
+    response = client.post("/fruits", json={"name": "orange"})
+    new_fruit = response.json()
+
+    assert response.status_code == 200
+    assert new_fruit["name"] == "orange"
+    assert fruits_db[new_fruit["id"]].name == "orange"
+
+
+def test_update_fruit():
+    response = client.put("/fruits/1", json={"name": "kiwi"})
+    updated_fruit = response.json()
+
+    assert response.status_code == 200
+    assert updated_fruit["name"] == "kiwi"
+    assert fruits_db[1].name == "kiwi"
+
+
+def test_delete_fruit():
+    response = client.delete("/fruits/1")
+    assert response.status_code == 200
+    assert 1 not in fruits_db
